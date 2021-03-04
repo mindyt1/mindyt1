8357ccfcc484cbdf939e9c2a646ccfb796fd12b4
Mindy Tran <mbtran@ucdavis.edu>
2021-03-04 10:00:29 -0800

    prototype

---
+++ des157/final/proto/script.js
@@ -0,0 +1,30 @@
+(function () {
+
+    'use strict';
+
+    // add script here
+    
+    const openBtns = document.querySelectorAll('.open');
+    const closeBtns = document.querySelectorAll('.close');
+
+    for(const eachBtn of openBtns) {
+        eachBtn.addEventListener('click', function(event){
+            event.preventDefault();
+            const thisBtn = event.target.id;
+            document.getElementById(`ol-${thisBtn}`).className = 'overlay showing';
+        });
+    }
+
+    for(const eachBtn of closeBtns) {
+        eachBtn.addEventListener('click', function(event){
+            event.preventDefault();
+            document.querySelector('.showing').className = 'overlay hidden';
+        });
+    }
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Escape') {
+            document.querySelector('.showing').className = 'overlay hidden';
+        }
+    })
+})();
